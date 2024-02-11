78342ba47131a7e87bad8b4097c4de389741cbc1
Shaylen Ukbush <shaylen.ukbush@gmail.com>
2024-02-11 12:00:59 +0200

    feat: add generic accounts

---
+++ accounts-application/Generic/Account.cs
@@ -0,0 +1,14 @@
+﻿namespace accounts_application.Generic
+{
+    public class Account
+    {
+        public long AccountId { get; }
+        public long Balance { get; set; }
+
+        public Account(long accountId, long balance)
+        {
+            AccountId = accountId;
+            Balance = balance;
+        }
+    }
+}
